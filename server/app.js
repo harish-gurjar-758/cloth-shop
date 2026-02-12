adf1235467a4bc1a0f6edeb9e1171c287248af4d
harish-gurjar-758 <gurjarharish758@gmail.com>
2026-02-12 10:31:35 +0530

    create app.js and connect the auth routes

---
+++ server/app.js
@@ -0,0 +1,15 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+import authRoutes from "./src/routes/auth.routes.js";
+
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+
+app.use("/api/auth", authRoutes);
+
+export default app;
