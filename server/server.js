5c7db73bafcda3f9abc0aaaa620386d93b003bc1
harish-gurjar-758 <gurjarharish758@gmail.com>
2026-02-12 10:32:51 +0530

    create server.js

---
+++ server/server.js
@@ -0,0 +1,11 @@
+import dotenv from "dotenv";
+import app from "./app.js";
+import { connectDB } from "./src/config/db.js";
+
+dotenv.config();
+
+connectDB();
+
+app.listen(process.env.PORT, () =>
+    console.log(`Server running on port ${process.env.PORT}`)
+);
