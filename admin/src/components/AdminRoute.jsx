654593d0c140e56e3986d0fd6b0c6e0efd1ff260
harish-gurjar-758 <gurjarharish758@gmail.com>
2026-02-13 15:44:10 +0530

    fix some bugs and create loader

--- admin/src/components/AdminRoute.jsx
+++ admin/src/components/AdminRoute.jsx
@@ -1,11 +1,12 @@
 import React, { useContext } from "react";
 import { Navigate } from "react-router-dom";
 import { AuthContext } from "../context/AuthContext";
+import Loader from "./Loader";
 
 export default function AdminRoute({ children }) {
     const { user, loading } = useContext(AuthContext);
 
-    if (loading) return <p>Loading...</p>;
+    if (loading) return <Loader />;
 
     if (!user || user.role !== "admin") {
         return <Navigate to="/" />;
